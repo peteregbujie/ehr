559fdfe0979a37d26a9e9631fc5debc9fd9b3242
Peter Egbujie <ogbillion@gmail.com>
2024-10-15 17:16:11 -0700

    chore(drizzle): Update Drizzle configuration for PostgreSQL dialect

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,22 +1,13 @@
+
 import { defineConfig } from "drizzle-kit";
 
-import * as dotenv from "dotenv";
-
-dotenv.config({
- path: ".env.local",
-});
-
-if (!process.env.DATABASE_URL) {
- throw new Error("DATABASE_URL is not defined.");
-}
-
 export default defineConfig({
+ schema: "./src/db/schema/index.ts",
  dialect: "postgresql",
+ out: "./src/db/migrations", 
  dbCredentials: {
   url: process.env.DATABASE_URL!,
- },
- schema: "./src/db/schema/index.ts",
- out: "./src/db/migrations",
+ }, 
  verbose: true,
  strict: true,
 });
