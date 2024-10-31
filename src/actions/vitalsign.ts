76c2c541349e2cb94efa96a9ca4320b7f2c22659
Peter Egbujie <ogbillion@gmail.com>
2024-10-30 19:23:05 -0700

    fix: update createVitalSignAction to include encounter_id

--- src/actions/vitalsign.ts
+++ src/actions/vitalsign.ts
@@ -1,21 +1,22 @@
 
 import {  authenticatedAction } from "@/lib/safe-action";
-import { selectVitalSignSchema } from "@/lib/validations/vitalsign";
+
 import { createVitalSignUseCase } from "@/use-cases/vitalsign";
 import { revalidatePath } from "next/cache";
 import { useServerPath } from "@/lib/utils";
+import { insertVitalSignSchema } from "@/db/schema/vitalsign";
 
 
 
 export const createVitalSignAction = authenticatedAction
   .createServerAction()
   .input(    
-    selectVitalSignSchema   
+    insertVitalSignSchema   
   )
-  .handler(async ({ ctx, input: {  height, weight, systolic_blood_pressure, diastolic_blood_pressure, heart_rate, body_temperature, respiratory_rate, oxygen_saturation, bmi, measured_on } }) => {
+  .handler(async ({ ctx, input: {  height, weight, systolic_pressure, diastolic_pressure, heart_rate, body_temperature, respiratory_rate, oxygen_saturation, bmi, encounter_id } }) => {
     const { path } = await useServerPath();
     await createVitalSignUseCase(ctx.user,{
-        height, weight, systolic_blood_pressure, diastolic_blood_pressure, heart_rate, body_temperature, respiratory_rate, oxygen_saturation, bmi, measured_on
+        height, weight, systolic_pressure, diastolic_pressure, heart_rate, body_temperature, respiratory_rate, oxygen_saturation, bmi,encounter_id
         
     });
     revalidatePath(`/patient/${path}`);
